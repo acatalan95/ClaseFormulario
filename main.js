667d22b749858cbe5555c20ca59b37017a8fa4a9
acatalan95 <ajosethcatalan@gmail.com>
2025-06-05 18:56:52 -0500

    First Commit Formulario

---
+++ main.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("name") 
+const appelido = document.getElementById("surname")
+
+function enviarFormulario(){
+    alert("nombre "+ nombre.value +"\napellido: "+ appelido.value)
+}
