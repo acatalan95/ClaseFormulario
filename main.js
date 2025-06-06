113334608898aade18ffc672630e7e9b6bce0f4e
acatalan95 <ajosethcatalan@gmail.com>
2025-06-05 19:52:43 -0500

    Third Commit Formulario

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const nombre = document.getElementById("name");
 const appelido = document.getElementById("surname");
 
+const nombreResultado = document.getElementById("respuestaNombre")
+const apellidoResultado = document.getElementById("respuestaApellido")
+
 function enviarFormulario(){
-    alert("nombre "+ nombre.value +"\napellido: "+ appelido.value);
+    //alert("nombre "+ nombre.value +"\napellido: "+ appelido.value);
+    //nombreResultado.innerText = "Nombre: " + nombre.value
+    nombreResultado.innerText = `Nombre: ${nombre.value}`
+    apellidoResultado.innerText = `Apellido: ${appelido.value}`
+    
 }
+
